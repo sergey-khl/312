1523998cc0a3a284921956b1334f72fd9fb6cd8b
Sergey <sergeykhlynovskiy@gmail.com>
2024-09-22 12:36:30 -0600

    part 5 done

--- lab1/code/lab1_problem5.py
+++ lab1/code/lab1_problem5.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
+"""
+Group Members: Sergey Khlynovskiy, Jerrica Yang
 
+Date: 2024/09/22
+ 
+Brick Number: G2
+
+Lab Number: 1
+
+Problem Number: 5
+ 
+Brief Program/Problem Description: 
+
+    Bot must either act cowardly (move to the side unless light right in front, in which case charge),
+    or act aggressive (always charge the light)
+
+Brief Solution Summary:
+
+	used 2 light sensors on either side of the robot to tell when the light is closer to one of the sensors
+    used this difference to either go away or charge at the robot. The distance to the light was used to
+    perform a charging maneuver. We used the self.on function to constantly move.
+
+Used Resources/Collaborators:
+    https://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/sensors.html
+    http://ugweb.cs.ualberta.ca/~vis/courses/robotics/assign/a1MobRob/lab1_notes.jpg
+
+I/we hereby certify that I/we have produced the following solution 
+using only the resources listed above in accordance with the 
+CMPUT 312 collaboration policy.
+"""
+
+import sys
 from time import sleep
 
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_2, INPUT_4
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, SpeedRPM, SpeedPercent, MoveTank
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank
 
 
 class Robot(MoveTank):
@@ -34,7 +65,7 @@ class Robot(MoveTank):
         right_speed = min(100, self.max_speed * left_light * self.margin)
         self.on(left_speed, right_speed)
 
-    def startEmotion(self, emotion="cowardice"):
+    def startEmotion(self, emotion):
         while True:
             if emotion == "cowardice":
                 self.doCowardice()
@@ -46,6 +77,18 @@ class Robot(MoveTank):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Error: Exactly one argument (aggression/cowardice) is required.")
+        sys.exit(1)
+    
+    input_arg = sys.argv[1]
+
+    if input_arg not in ["aggression", "cowardice"]:
+        print("Error: Argument must be 'aggression' or 'cowardice'.")
+        sys.exit(1)
+
     robo = Robot(OUTPUT_B, OUTPUT_A)
-    # dead reckoning commands
-    robo.startEmotion("aggression")
+    if input_arg == "aggression":
+        robo.startEmotion("aggression")
+    else:
+        robo.startEmotion("cowardice")
